78dbe00db75e69ad8e532a5b1f074437d11c7124
Anthony Varkey <anthonyvarkey@gmail.com>
2023-11-18 08:53:30 -0500

    Added Tailwind also

---
+++ static/HelloWorld.js
@@ -0,0 +1,13 @@
+import { define, html } from 'https://esm.sh/hybrids@^8';
+   
+function changeIt(host){
+    console.log(host.name);
+    var val = host.name;
+    host.name = (val == "Joe") ? "World" : "Joe";
+}
+
+define({
+    tag: "hello-world",
+    name: '',
+    content: (host) => { return html`<button class="bg-blue-100 p-5 rounded-sm" onclick="${()=>changeIt(host)}">Hello ${host.name}!</button>`},
+});
